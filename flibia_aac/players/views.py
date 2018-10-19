ca3f395970ed75ded3e3b7c9dc83fb49d6bcd94e
Thomaz Soares <thomazthz0@gmail.com>
2018-10-19 12:35:57 -0300

    Add 'queries' module and refactor 'highscores' view

--- flibia_aac/players/views.py
+++ flibia_aac/players/views.py
@@ -8,40 +8,14 @@ from sqlalchemy import or_
 from sqlalchemy.orm import load_only, joinedload
 from sqlalchemy.sql import func
 
-from ..db import db
+from flibia_aac.db import db
+from flibia_aac.queries import get_highscores
 
 from .forms import CharacterCreationForm, SearchForm
-from .models import Player, PlayersOnline, PlayerDeaths, Vocation
+from .models import Player, PlayersOnline, PlayerDeaths
+
 
 player = Blueprint('player', __name__, url_prefix='/character')
-
-
-def _vocation_filter_builder(vocation, promoted_vocation=None):
-    if not promoted_vocation:
-        return Player.vocation==Vocation[vocation].value
-    return or_(Player.vocation==Vocation[vocation].value,
-               Player.vocation==Vocation[promoted_vocation].value)
-
-HIGHSCORES_CATEGORIES = {
-    'experience': 'experience',
-    'magic_level': 'maglevel',
-    'fist': 'skill_fist',
-    'club': 'skill_club',
-    'sword': 'skill_sword',
-    'axe': 'skill_axe',
-    'dist': 'skill_dist',
-    'shielding': 'skill_shielding',
-    'fishing': 'skill_fishing',
-}
-
-VOCATIONS_FILTERS = {
-    'sorcerer': _vocation_filter_builder('SORCERER', 'MASTER_SORCERER'),
-    'druid': _vocation_filter_builder('DRUID', 'ELDER_DRUID'),
-    'paladin': _vocation_filter_builder('PALADIN', 'ROYAL_PALADIN'),
-    'knight': _vocation_filter_builder('KNIGHT', 'ELITE_KNIGHT'),
-    'rookie': _vocation_filter_builder('NONE'),
-    'all': None,
-}
 
 
 @player.route('/create', methods=['GET', 'POST'])
@@ -58,7 +32,6 @@ def create_character():
     return redirect(url_for('account.detail'))
 
 
-
 @player.route('/profile', methods=['GET'])
 def profile():
     form = SearchForm()
@@ -71,19 +44,14 @@ def profile():
 
 @player.route('/highscores', methods=['GET', 'POST'])
 def highscores():
-    highscore_category = request.args.get('highscore_category', 'experience')
-    vocation_filter = request.args.get('vocation', None)
+    category = request.args.get('highscore_category', 'experience')
+    vocation = request.args.get('vocation', None)
 
-    highscore_category = HIGHSCORES_CATEGORIES.get(highscore_category)
-    vocation_filter = VOCATIONS_FILTERS.get(vocation_filter, None)
+    players = get_highscores(category, vocation)
 
-    field = getattr(Player, highscore_category)
-    players = Player.query.options(load_only(field))
-
-    if vocation_filter is not None:
-        players = players.filter(vocation_filter)
-
-    return render_template('players/highscores.html', players=players, highscore_category=highscore_category)
+    return render_template('players/highscores.html',
+                           players=players,
+                           highscore_category=category)
 
 
 @player.route('/players_online', methods=['GET'])
---
+++ flibia_aac/queries.py
@@ -0,0 +1,47 @@
+from sqlalchemy import or_
+from sqlalchemy.orm import load_only
+
+from .players.models import Player, Vocation
+
+
+def _vocation_filter_builder(vocation, promoted_vocation=None):
+    if not promoted_vocation:
+        return Player.vocation==Vocation[vocation].value
+    return or_(Player.vocation==Vocation[vocation].value,
+               Player.vocation==Vocation[promoted_vocation].value)
+
+
+VOCATIONS_FILTERS = {
+    'sorcerer': _vocation_filter_builder('SORCERER', 'MASTER_SORCERER'),
+    'druid': _vocation_filter_builder('DRUID', 'ELDER_DRUID'),
+    'paladin': _vocation_filter_builder('PALADIN', 'ROYAL_PALADIN'),
+    'knight': _vocation_filter_builder('KNIGHT', 'ELITE_KNIGHT'),
+    'rookie': _vocation_filter_builder('NONE'),
+    'all': None,
+}
+
+
+HIGHSCORES_CATEGORIES = {
+    'experience': 'experience',
+    'magic_level': 'maglevel',
+    'fist': 'skill_fist',
+    'club': 'skill_club',
+    'sword': 'skill_sword',
+    'axe': 'skill_axe',
+    'dist': 'skill_dist',
+    'shielding': 'skill_shielding',
+    'fishing': 'skill_fishing',
+}
+
+
+def get_highscores(category, vocation=None, limit=None):
+    highscore_category = HIGHSCORES_CATEGORIES.get(category)
+    vocation_filter = VOCATIONS_FILTERS.get(vocation, None)
+
+    field = getattr(Player, highscore_category)
+    players = Player.query.options(load_only(field))
+
+    if vocation_filter is not None:
+        players = players.filter(vocation_filter)
+
+    return players
